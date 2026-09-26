38116f4cdb75c685044d88ebe1e1a25c1717dbfc
petruho2019 <vladgolovko20062018@mail.ru>
2026-09-26 11:01:30 +0300

    refactor

--- src/Entities/HtmlElement.cs
+++ src/Entities/HtmlElement.cs
@@ -1,12 +1,9 @@
-using AngleSharp.Dom;
-using Riok.Mapperly.Abstractions;
-
 namespace ParsingHTML.Entities;
 
 
 public class HtmlElement
 {
-    public string? Id { get; set; }
-    public string? AttributeValue { get; set; }
-    public string? HtmlValue { get; set; }
+    public string Id { get; set; }
+    public string AttributeValue { get; set; }
+    public string HtmlValue { get; set; }
 }
